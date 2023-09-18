0d2e0184bd1ac0d23887d05458355177335cb364
M.S.T.O.P <mstop4@gmail.com>
2023-09-18 17:28:55 -0400

    add room and path data

--- src/setup.ts
+++ src/setup.ts
@@ -3,10 +3,52 @@ import { Line2 } from 'three/addons/lines/Line2.js';
 import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
 import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
 
+import pathsData from './data/paths.json';
+import roomsData from './data/rooms.json';
+
+const camX = 30;
+const camY = 100;
+const camZ = 30;
+
 export type AnimateParams = {
   scene: THREE.Scene,
   camera: THREE.PerspectiveCamera,
   renderer: THREE.WebGLRenderer,
+}
+
+export type PathData = {
+  type: string,
+  points: number[][],
+}
+
+export type RoomData = {
+  type: string,
+  corners: number[][],
+}
+
+export function createPath(pathData: PathData, material: LineMaterial) {
+  const { points: rawPoints } = pathData;
+  const points = rawPoints.flat(1);
+
+  const lineGeometry = new LineGeometry().setPositions( points );
+  const line = new Line2( lineGeometry, material );
+  line.computeLineDistances();
+	line.scale.set( 1, 1, 1 );
+  return line;
+}
+
+export function createRoom(roomData: RoomData, material: THREE.MeshBasicMaterial) {
+  const { corners } = roomData;
+  const width = Math.abs(corners[1][0] - corners[0][0]);
+  const length = Math.abs(corners[1][1] - corners[0][1]);
+  const height = Math.abs(corners[1][2] - corners[0][2]);
+
+  const cubeGeometry = new THREE.BoxGeometry( width, length, height );
+  const cube = new THREE.Mesh( cubeGeometry, material );
+  cube.position.x = corners[0][0] + width / 2;
+  cube.position.y = corners[0][1] + length / 2;
+  cube.position.z = corners[0][2] + height / 2;
+  return cube;
 }
 
 export function setupScene(): AnimateParams {
@@ -18,27 +60,21 @@ export function setupScene(): AnimateParams {
   document.body.appendChild( renderer.domElement );
 
   // cube
-  const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
   const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-  const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+  const cube = createRoom(roomsData[0], cubeMaterial);
   scene.add( cube );
 
   // lines
   const lineMaterial = new LineMaterial( { color: 0x0000ff, linewidth: 0.25 } );
   lineMaterial.worldUnits = true;
-  const points = [];
-  points.push(-2, 0, 0);
-  points.push(0, 2, 0);
-  points.push(2, 0, 0);
+  
+  for (let path of pathsData) {
+    const line = createPath(path, lineMaterial);
+    scene.add(line);
+  }
 
-  const lineGeometry = new LineGeometry().setPositions( points );
-  const line = new Line2( lineGeometry, lineMaterial );
-  line.computeLineDistances();
-	line.scale.set( 1, 1, 1 );
-  scene.add(line);
-
-  camera.position.set(5, 5, 5);
-  camera.lookAt(0, 0, 0);
+  camera.position.set(camX, camY, camZ);
+  camera.lookAt(camX - 1, camY - 1, camZ - 1);
 
   return {
     scene,
