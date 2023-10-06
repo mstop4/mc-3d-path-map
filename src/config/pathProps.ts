30d5babce08c73ef4b0f58a92801780ec8cd00bb
M.S.T.O.P <mstop4@gmail.com>
2023-10-06 17:09:51 -0400

    install colorbrewer

--- src/config/pathProps.ts
+++ src/config/pathProps.ts
@@ -1,68 +1,75 @@
+import colorbrewer from 'colorbrewer';
 import {
   DefaultPathPropertyDefinitions,
   SimplePathPropertyDefinitions,
 } from './pathProps.types';
 
+const colourScheme = colorbrewer.Set1[9];
+
+function removePoundSign(rgbHexCode: string) {
+  return rgbHexCode.replace('#', '');
+}
+
 export const defaultPathProps: DefaultPathPropertyDefinitions = {
   ogTunnel: {
     name: 'Surface Tunnel',
-    colour: '984ea3',
+    colour: removePoundSign(colourScheme[6]),
     cbfColour: '2166ac',
     isExterior: false,
     isNatural: false,
   },
   ugTunnel: {
     name: 'Underground Tunnel',
-    colour: '377eb8',
-    cbfColour: '4393c3',
+    colour: removePoundSign(colourScheme[1]),
+    cbfColour: '87CEFA',
     isExterior: false,
     isNatural: false,
   },
   cBridge: {
     name: 'Covered Bridge',
-    colour: '4daf4a',
+    colour: removePoundSign(colourScheme[2]),
     cbfColour: '92c5de',
     isExterior: false,
     isNatural: false,
   },
   oBridge: {
     name: 'Open Bridge',
-    colour: 'ffff33',
+    colour: removePoundSign(colourScheme[5]),
     cbfColour: 'd1e5f0',
     isExterior: true,
     isNatural: false,
   },
   exPath: {
     name: 'Open Path',
-    colour: 'e41a1c',
+    colour: removePoundSign(colourScheme[0]),
     cbfColour: 'e41a1c',
     isExterior: true,
     isNatural: true,
   },
   nCave: {
     name: 'Cave',
-    colour: 'ff7f00',
+    colour: removePoundSign(colourScheme[4]),
     cbfColour: 'fddbc7',
     isExterior: false,
     isNatural: true,
   },
   ladder: {
     name: 'Ladder',
-    colour: 'cccccc',
+    colour: removePoundSign(colourScheme[8]),
     cbfColour: 'f7f7f7',
     isExterior: true,
     isNatural: false,
   },
   bastion: {
     name: 'Bastion',
-    colour: 'a65628',
+    colour: removePoundSign(colourScheme[3]),
     cbfColour: 'f4a582',
     isExterior: true,
     isNatural: true,
   },
   nFortress: {
     name: 'Nether Fortress',
-    colour: 'f781bf',
+    colour: removePoundSign(colourScheme[7]),
     cbfColour: 'd6604d',
     isExterior: true,
     isNatural: true,
