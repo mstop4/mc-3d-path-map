72fde9c3e4e561ec14d52891388e90318bff4190
M.S.T.O.P <mstop4@gmail.com>
2023-12-28 18:14:44 -0500

    add label filter

--- src/components/objects/gui.config.ts
+++ src/components/objects/gui.config.ts
@@ -5,6 +5,12 @@ export const allColourModeKeys: Record<string, string> = {
   nat: 'Natural/Artificial',
 };
 
+export const allLabelFilters: Record<string, string> = {
+  none: 'None',
+  enderChests: 'Ender Chests',
+  cherryTrees: 'Cherry Trees',
+};
+
 export const allCameraPositionsKeys: Record<string, string> = {
   demo: 'Demo',
   isometric: 'Isometric',
@@ -14,10 +20,19 @@ export const allCameraPositionsKeys: Record<string, string> = {
 };
 
 export const colourModesAvailable = ['default', 'cbf', 'ext', 'nat'];
+export const labelFiltersAvailable = ['none', 'enderChests', 'cherryTrees'];
 
 export const activeColourModes = colourModesAvailable.reduce(
   (obj: Record<string, string>, key) => {
     obj[key] = allColourModeKeys[key];
+    return obj;
+  },
+  {},
+);
+
+export const activeLabelFilters = labelFiltersAvailable.reduce(
+  (obj: Record<string, string>, key) => {
+    obj[key] = allLabelFilters[key];
     return obj;
   },
   {},
