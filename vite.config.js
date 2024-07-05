a3a3825c4bed058dbb7d294739cd860596c4bd1d
M.S.T.O.P <mstop4@gmail.com>
2024-07-05 18:45:30 -0400

    change vite config so top level await can be used

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/mc-3d-path-map/'
+  base: '/mc-3d-path-map/',
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  }
 });
